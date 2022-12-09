1e5c92b807b6dde2745bdaed235d6b4547355a00
Michael Senn <michael@morrolan.ch>
2022-12-09 11:37:47 +0100

    WIP commit

--- punchscan/presentation/pdfpc.sh
+++ punchscan/presentation/pdfpc.sh
@@ -4,5 +4,5 @@
 # --notes Location of notes on ultrawide PDF
 # --presenter-screen screen to use for presenter overview (next slide, notes)
 # --presentation-screen screen to use for presentation (fullscreen slide)
-# pdfpc --duration=35 --presenter-screen=eDP-1 --presentation-screen=DP-1 punchscan.pdf
-pdfpc --duration=35 -S punchscan.pdf
+pdfpc --duration=35 --presenter-screen=eDP-1 --presentation-screen=DP-1 punchscan.pdf
+# pdfpc --duration=35 -S punchscan.pdf
